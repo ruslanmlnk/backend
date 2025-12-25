27d0636bb5b03b2e73b9a16a45bec3fac739b126
Ruslan Melnyk <kanevgoga2@gmail.com>
2025-12-25 14:13:44 +0200

    фцвуц

---
+++ src/migrations/20251225_120809.ts
@@ -0,0 +1,18 @@
+import { sql } from '@payloadcms/db-postgres'
+import type { MigrateUpArgs, MigrateDownArgs } from '@payloadcms/db-postgres'
+
+export async function up({ payload }: MigrateUpArgs): Promise<void> {
+  await payload.db.drizzle.execute(sql`
+    ALTER TABLE "products"
+      ADD COLUMN IF NOT EXISTS "meta_meta_title" varchar,
+      ADD COLUMN IF NOT EXISTS "meta_meta_description" varchar;
+  `)
+}
+
+export async function down({ payload }: MigrateDownArgs): Promise<void> {
+  await payload.db.drizzle.execute(sql`
+    ALTER TABLE "products"
+      DROP COLUMN IF EXISTS "meta_meta_title",
+      DROP COLUMN IF EXISTS "meta_meta_description";
+  `)
+}
--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20251221_add_contact_requests_lock from './20251221_add_contact_requests_lock';
+import * as migration_20251225_120809 from './20251225_120809';
 
 export const migrations = [
   {
     up: migration_20251221_add_contact_requests_lock.up,
     down: migration_20251221_add_contact_requests_lock.down,
-    name: '20251221_add_contact_requests_lock'
+    name: '20251221_add_contact_requests_lock',
+  },
+  {
+    up: migration_20251225_120809.up,
+    down: migration_20251225_120809.down,
+    name: '20251225_120809'
   },
 ];
